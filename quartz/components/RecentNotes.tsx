fa8adcfa7843de22ef330d31e4de46d74ee52a05
Borys Borysenko <3684918+bborysenko@users.noreply.github.com>
2024-05-18 22:36:29 +0000

    feat: display recent notes only on home page
    https://github.com/jackyzha0/jackyzha0.github.io
    https://github.com/TheRealMattDunn/mattdunn.info

--- quartz/components/RecentNotes.tsx
+++ quartz/components/RecentNotes.tsx
@@ -30,6 +30,9 @@ export default ((userOpts?: Partial<Options>) => {
     displayClass,
     cfg,
   }: QuartzComponentProps) => {
+    if (fileData.slug !== "index") {
+      return <></>
+    }
     const opts = { ...defaultOptions(cfg), ...userOpts }
     const pages = allFiles.filter(opts.filter).sort(opts.sort)
     const remaining = Math.max(0, pages.length - opts.limit)
